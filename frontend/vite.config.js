fbac8e47f05f96ef4fa6a7497bd0ebfb03829936
enmanueljose <enmanuelrondon54@gmail.com>
2025-02-02 17:56:16 -0400

    tercer commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,19 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   server: { port: 5173 }
-// })
+export default defineConfig({
+  plugins: [react()],
+  server: { port: 5173 }
+})
 
-export default {
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://outfiro-er-backend-cmaixboyb-enmanuelrondon1s-projects.vercel.app',
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
-};
